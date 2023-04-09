4cc89cd43572816bc95f13762e5bcc43e55177c3
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-09 01:50:18 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,16 +2,33 @@ import streamlit as st
 import pandas as pd
 
 
+
 st.set_page_config(
     page_title="APP Geocodificación",
 )
 
+
+
 st.write("# APP Geocodificación")
 
-st.markdown("""""" )
+st.markdown("")
+st.markdown("")
 
 st.markdown(
     """
-    ...
+Esta aplicación permite utilizar distintos servicios de geocodificación en un mismo sitio web,
+basándose principalmente en la utilización del cliente de python Geopy (https://geopy.readthedocs.io/en/stable/#).
+
+Por otro lado, se facilita documentación y una herramienta para validar los resultados
+que se obtienen a partir del uso de los servicios de geocodificación.
+
+<br>
+
+IMPORTANTE: Esta aplicación, al igual que Geopy, no es un servicio. <br>
+Citando a Geopy: <br>
+La codificación geográfica es proporcionada por una serie de servicios diferentes, que no están afiliados a geopy de ninguna manera.
+Estos servicios proporcionan API, que cualquiera podría implementar, y geopy es solo una biblioteca que proporciona estas implementaciones
+para muchos servicios diferentes en un solo paquete.
+
 """
-)
+, unsafe_allow_html=True)
