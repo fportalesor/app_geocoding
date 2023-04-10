279641b47f09c3d8718c10f1f9106931cc0ebf71
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-10 15:22:04 -0400

    Update 9_Revision_resultados.py

--- pages/9_Revision_resultados.py
+++ pages/9_Revision_resultados.py
@@ -85,8 +85,8 @@ def std_midas(df):
     df["latitud"] = df["lat1"] + "," + df["lat2"]
     df["longitud"] = df["long1"] + "," + df["long2"]
     
-    #df["lat"] = df["lat1"] + "." + df["lat2"]
-    #df["long"] = df["long1"] + "." + df["long2"]
+    df["lat"] = df["lat1"] + "." + df["lat2"]
+    df["long"] = df["long1"] + "." + df["long2"]
 
     df['long'] = df['long'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
     df['lat'] = df['lat'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
