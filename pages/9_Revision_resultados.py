e9d842d3d67e681a9bc37987e4e0188b34a283a0
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-10 15:12:23 -0400

    Update 9_Revision_resultados.py

--- pages/9_Revision_resultados.py
+++ pages/9_Revision_resultados.py
@@ -88,8 +88,11 @@ def std_midas(df):
     df["lat"] = df["lat1"] + "." + df["lat2"]
     df["long"] = df["long1"] + "." + df["long2"]
 
-    df['long'] = df['long'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
-    df['lat'] = df['lat'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+    #df['long'] = df['long'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+    #df['lat'] = df['lat'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+   
+    df['long'] = df['long'].str.replace(',', '.', regex=True).astype(float)
+    df['lat'] = df['lat'].str.replace(',', '.', regex=True).astype(float)
 
     puntos = gpd.GeoDataFrame(
         df, geometry=gpd.points_from_xy(df.long , df.lat))
