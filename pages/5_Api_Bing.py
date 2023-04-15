390fc6338a8db116462911f80df01eb6a73cdfb7
fportales <68240613+fportalesor@users.noreply.github.com>
2023-04-15 13:55:56 -0400

    Update 5_Api_Bing.py

--- pages/5_Api_Bing.py
+++ pages/5_Api_Bing.py
@@ -157,7 +157,7 @@ with container:
             key='download-csv-bing')
 
 
-link_rev = '[Revision resultados](http://localhost:8501/Revision_resultados)'
+link_rev = '[Revision resultados](https://fportalesor-app-geocoding-main-z8y7n3.streamlit.app/Revision_resultados)'
 
 with st.sidebar:
         st.markdown("#### Instrucciones de uso")
