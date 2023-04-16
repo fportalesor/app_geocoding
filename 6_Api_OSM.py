0ebf947e59a2da991b56ef32dac01d4e3eaeb8f0
fportalesor <fportales.or@gmail.com>
2023-04-16 16:04:54 -0400

    Inicio

---
+++ 6_Api_OSM.py
@@ -0,0 +1,167 @@
+import streamlit as st
+import pandas as pd
+from geopy.geocoders import Nominatim
+import unidecode
+import numpy as np
+import geopandas as gpd
+import plotly.express as px
+import time
+
+
+st.set_page_config("Geocodificación API OSM-Nominatim")
+
+st.markdown("""<p style="text-align:center;"><img src="https://images.squarespace-cdn.com/content/v1/5616ded1e4b0e8660091fc57/1631816707277-CMQLTDY6CQXKO9FTWS78/OpenStreetMap.png"
+alt="whatever" width="240" height= "140"></p>""", unsafe_allow_html=True)
+
+st.markdown("---")
+
+df_ejemplo = pd.read_csv("archivo_ejemplo.csv", dtype=str, sep=";", encoding="latin-1")
+
+def convert_df(df):
+   return df.to_csv(index=False, sep=";").encode('latin-1')
+
+csv = convert_df(df_ejemplo)
+
+@st.cache_data()
+def CONSULTA_API_OSM(api_key_input, df_input):
+    api_key = api_key_input
+    df = df_input
+    df['direccion2'] = df['direccion'].apply(lambda x: unidecode.unidecode(x))
+    df['direccion2']= df['direccion2'].replace('[^a-zA-Z0-9 ]', '', regex=True)
+    df['comuna'] = df['comuna'].apply(lambda x: unidecode.unidecode(x))
+    df['comuna']= df['comuna'].replace('[^a-zA-Z0-9 ]', '', regex=True)
+    df['comuna'] = df['comuna'].str.title()
+    df['comuna'] = df['comuna'].replace(r'\s+', ' ', regex=True)
+    df['comuna'] = df['comuna'].replace(r"^ +| +$", r"", regex=True)
+    df['direccion_completa'] = df['direccion2'] + ", " + df['comuna'] +", "+ df['region'] + ", Chile"
+    df['direccion_completa']= df['direccion_completa'].apply(lambda x: unidecode.unidecode(x))
+
+    df0 = df.drop_duplicates(subset=['direccion'])
+
+    def get_address(x):
+        if hasattr(x,'address') and (x.address is not None): 
+            return x.address
+    
+    def get_latitude(x):
+        if hasattr(x,'latitude') and (x.latitude is not None): 
+            return x.latitude
+
+    def get_longitude(x):
+        if hasattr(x,'longitude') and (x.longitude is not None): 
+            return x.longitude
+
+    def get_OSM_type(x):
+        if hasattr(x,'raw') and (x.raw['class'] is not None): 
+            return x.raw['class']
+
+    geolocator = Nominatim(user_agent=api_key, timeout=5)
+    geolocate_column = df0['direccion_completa'].apply(geolocator.geocode)
+    df0['direccion_api'] = geolocate_column.apply(get_address)
+    df0['tipo_ubicacion'] = geolocate_column.apply(get_OSM_type)
+    df0['lat'] = geolocate_column.apply(get_latitude)
+    df0['long'] = geolocate_column.apply(get_longitude)
+
+    df0.drop(['direccion2'],axis='columns', inplace=True)
+    df0 = (df0[["direccion_completa", "direccion_api", "tipo_ubicacion", "lat", "long"]])
+
+    df1 = pd.merge(df, df0, on="direccion_completa", how="left")
+    df1.drop(['direccion2'],axis='columns', inplace=True)
+
+    df1['long'] = df1['long'].astype(str)
+    df1['lat'] = df1['lat'].astype(str)
+
+    df1["lat1"] = df1["lat"].str[:3].astype(str)
+    df1["long1"] = df1["long"].str[:3].astype(str)
+
+    df1["lat2"] = df1["lat"].str[4:10].astype(str)
+    df1["long2"] = df1["long"].str[4:10].astype(str)
+
+    df1["latitud"] = df1["lat1"] + "," + df1["lat2"]
+    df1["longitud"] = df1["long1"] + "," + df1["long2"]
+
+    df1['long'] = pd.to_numeric(df1['long'],errors='coerce')
+    df1['lat'] = pd.to_numeric(df1['lat'],errors='coerce')
+
+    puntos = gpd.GeoDataFrame(
+    df1, geometry=gpd.points_from_xy(df1.long , df1.lat))
+    puntos.set_crs(epsg=4326, inplace=True)
+
+    url = "https://www.dropbox.com/s/1n6fzddsb38bok5/comunas_chile_geo.zip?dl=1"
+
+    comunas = gpd.read_file(url)
+
+    puntos_j = gpd.sjoin(puntos, comunas, how="left", predicate='intersects')
+
+    join = pd.DataFrame(puntos_j)
+
+    join.drop(['geometry', 'index_right', 'lat1', 'lat2', 'long1', 'long2'], axis='columns', inplace=True)
+
+    join['comuna_geo'] = join['comuna_geo'].astype(str)
+    join['comuna_geo'] = join['comuna_geo'].apply(lambda x: unidecode.unidecode(x))
+
+    join['comunas_rev'] = np.where(join['comuna'] == join['comuna_geo'], "coincide", "no coincide")
+
+    join["api_consulta"] = 'OSM'
+
+    join = join[["id", "direccion","direccion_completa", "direccion_api", "tipo_ubicacion", "comuna",
+                              "comuna_geo", "comunas_rev","lat", "long", "latitud", "longitud", "api_consulta"]]               
+
+    return join
+
+
+
+def display_map(df):
+    fig = px.scatter_mapbox(df, lat='lat', lon='long', hover_name="id",
+                            hover_data=["direccion_completa", "direccion_api", "tipo_ubicacion"], 
+                            color="tipo_ubicacion",zoom=10, mapbox_style= 'carto-positron')
+    return fig
+
+container = st.container()
+
+with container:
+    st.download_button(
+        "Descargue archivo de referencia",
+        csv,
+        "Archivo de referencia.csv",
+        "text/csv",
+        key='download-csv-OSM')
+
+    uploaded_file = st.file_uploader("Elija un archivo csv para realizar la geocodificación", type="csv")
+
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file, dtype=str, sep=";", encoding="latin-1")
+    
+    api_key_input = st.text_input(label="Ingrese correo electrónico")
+    if api_key_input:
+        if st.button('Comenzar'):
+            st.write("Comienza la geocodificación...")
+            join = CONSULTA_API_OSM(api_key_input, df)
+
+            with st.spinner('El proceso está terminando...'):
+                time.sleep(5)
+            st.success('Listo!')
+            st.plotly_chart(display_map(join), use_container_width=True)
+
+            csv_salida = convert_df(join)
+
+            st.download_button(
+            "Descargue resultado",
+            csv_salida,
+            "resultado_API_OSM.csv",
+            "text/csv",
+            key='download-osm-here')
+
+
+link_rev = '[Revision resultados](https://fportalesor-app-geocoding-main-z8y7n3.streamlit.app/Revision_resultados)'
+
+with st.sidebar:
+        st.markdown("#### Instrucciones de uso")
+        st.markdown(f"""##### 
+1) Descargar archivo de referencia para cargar datos con el formato solicitado.
+2) Dar click en "Browse files" o en "Drag and drop file here" para cargar el archivo csv con los datos.
+3) Ingresar correo electrónico y luego presionar Enter.
+4) Dar click en el botón Comenzar.
+5) Se mapean los resultados obtenidos.
+6) Dar click en "Descargue resultado". 
+7) Puede visitar el módulo "{link_rev}" para utilizar la herramienta de validación disponible."""
+, unsafe_allow_html=True)
---
+++ 9_Revision_resultados.py
@@ -0,0 +1,252 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import unidecode
+import geopandas as gpd
+import numpy as np
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode, JsCode
+#import json
+#import osmnx as ox
+
+st.set_page_config("Revisión de resultados", layout="wide")
+
+st.markdown("# Revisión de resultados")
+
+st.markdown("")
+
+def convert_df(df):
+   return df.to_csv(index=False, sep=";").encode('latin-1')
+
+
+@st.cache_data()
+def display_map(df):
+    #url= "https://www.dropbox.com/s/3pb9xfuqlcx619a/limites_com_chile_dpa.zip?dl=1"
+    #comunas = gpd.read_file(url)
+    #pais = "Chile"
+    #comuna = df.iloc[0]["comuna_geo"]
+    #place_name = comuna + " , " + pais 
+    #area = ox.geocode_to_gdf(place_name)
+    fig = px.scatter_mapbox(df, lat='lat', lon='long', color_discrete_sequence= ["red"],
+                            hover_data=["direccion_completa", "direccion_api", "tipo_ubicacion"], hover_name="id", 
+                            color="tipo_ubicacion",zoom=17, mapbox_style= 'open-street-map', height=500)
+    fig.update_traces(marker={'size': 15})
+    #fig.update_layout(
+    #    mapbox={
+    #       "layers": [
+    #           {
+    #               "source": json.loads(area.geometry.to_json()),
+    #               "below": "traces",
+    #               "type": "line",
+    #               "color": "purple",
+    #                "line": {"width": 1.5},
+    #           }]})
+    
+    return fig
+
+@st.cache_data()
+def std_app(df):
+    condicion1 = df['comuna'] == df['comuna_geo']
+    lista_validas = ["{'accuracy': 'point'}", "{'accuracy': 'interpolated'}", "ROOFTOP", "RANGE_INTERPOLATED",
+                      "Point Address", "Address Range", "houseNumber", "Address", "building", "POINT", "ADDRESS",
+                      "PointAddress", "StreetAddress", "StreetAddressExt"]
+    condicion2 = df['tipo_ubicacion'].isin(lista_validas)   
+    df["ubicación_válida"] = np.where((condicion1) & (condicion2), True, False)
+    df = df[["ubicación_válida","id","direccion", "direccion_completa", "direccion_api", "tipo_ubicacion", "comuna",
+                              "comuna_geo", "comunas_rev","lat", "long", "latitud", "longitud", "api_consulta"]]
+    
+    df['long'] = df['longitud'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+    df['lat'] = df['latitud'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+   
+    return df
+
+
+@st.cache_data()
+def std_midas(df):
+    cols = ['id', 'via', 'region', 'comuna', 'direccion', 'direccion_completa', 'lat', 'long', 'direccion_api', 'tipo_ubicacion', 'otro']
+    df.columns = cols
+    df['direccion_completa'] = df['direccion_completa'].apply(lambda x: unidecode.unidecode(x))
+    df['direccion_completa']= df['direccion_completa'].replace('[^a-zA-Z0-9 ]', '', regex=True)
+    df['comuna'] = df['comuna'].apply(lambda x: unidecode.unidecode(x))
+    df['comuna']= df['comuna'].replace('[^a-zA-Z0-9 ]', '', regex=True)
+    df['comuna'] = df['comuna'].str.title()
+    df['comuna'] = df['comuna'].replace(r'\s+', ' ', regex=True)
+    df['comuna'] = df['comuna'].replace(r"^ +| +$", r"", regex=True)
+    
+    df['long'] = df['long'].astype(str)
+    df['lat'] = df['lat'].astype(str)
+
+    df["lat1"] = df["lat"].str[:3].astype(str)
+    df["long1"] = df["long"].str[:3].astype(str)
+
+    df["lat2"] = df["lat"].str[4:12].astype(str)
+    df["long2"] = df["long"].str[4:12].astype(str)
+      
+    df["latitud"] = df["lat1"] + "," + df["lat2"]
+    df["longitud"] = df["long1"] + "," + df["long2"]
+     
+    #df["lat"] = pd.to_numeric(df["lat1"] + "." + df["lat2"])
+    #df["long"] = pd.to_numeric(df["long1"] + "." + df["long2"])
+
+    #df['long'] = df['long'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+    #df['lat'] = df['lat'].str.replace('.','', regex=True).str.replace(',', '.', regex=True).astype(float)
+   
+    #df['long'] = df['longitud'].str.replace(',', '.', regex=True)
+    #df['lat'] = df['latitud'].str.replace(',', '.', regex=True)
+    #df['long'] = df['long'].str.replace('.','', regex=True).astype(float)
+    #df['lat'] = df['lat'].str.replace('.','', regex=True).astype(float)
+
+    puntos = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.long , df.lat))
+    puntos.set_crs(epsg=4326, inplace=True)
+
+    url = "https://www.dropbox.com/s/1n6fzddsb38bok5/comunas_chile_geo.zip?dl=1"
+
+    comunas = gpd.read_file(url)
+
+    puntos_j = gpd.sjoin(puntos, comunas, how="left", predicate='intersects')
+
+    join = pd.DataFrame(puntos_j)
+
+    join.drop(['geometry', 'index_right', 'lat1', 'lat2', 'long1', 'long2'], axis='columns', inplace=True)
+                
+    join['comuna_geo'] = join['comuna_geo'].astype(str)
+    join['comuna_geo'] = join['comuna_geo'].apply(lambda x: unidecode.unidecode(x))
+
+
+    join['comunas_rev'] = np.where(join['comuna'] == join['comuna_geo'], "coincide", "no coincide")     
+
+    join["api_consulta"] = ''
+
+    condicion1 = join['comuna'] == join['comuna_geo']
+    lista_validas = ["{'accuracy': 'point'}", "{'accuracy': 'interpolated'}", "ROOFTOP", "RANGE_INTERPOLATED",
+                      "Point Address", "Address Range", "houseNumber", "Address", "building", "POINT", "ADDRESS",
+                      "PointAddress", "StreetAddress", "StreetAddressExt"]
+    condicion2 = join['tipo_ubicacion'].isin(lista_validas)
+    join["ubicación_válida"] = np.where((condicion1) & (condicion2), True, False)
+    join = join[["ubicación_válida","id","direccion", "direccion_completa", "direccion_api", "tipo_ubicacion", "comuna",
+                              "comuna_geo", "comunas_rev","lat", "long", "latitud", "longitud", "api_consulta"]]
+    
+    return join
+
+checkbox_renderer = JsCode("""
+class CheckboxRenderer{
+
+    init(params) {
+        this.params = params;
+
+        this.eGui = document.createElement('input');
+        this.eGui.type = 'checkbox';
+        this.eGui.checked = params.value;
+
+        this.checkedHandler = this.checkedHandler.bind(this);
+        this.eGui.addEventListener('click', this.checkedHandler);
+    }
+
+    checkedHandler(e) {
+        let checked = e.target.checked;
+        let colId = this.params.column.colId;
+        this.params.node.setDataValue(colId, checked);
+    }
+
+    getGui(params) {
+        return this.eGui;
+    }
+
+    destroy(params) {
+    this.eGui.removeEventListener('click', this.checkedHandler);
+    }
+}//end class
+""")
+
+opciones_csv = st.radio(
+    "¿Cúal tipo de archivo usarás?",
+    ('Por defecto APP', 'Geomasiva Midas'))
+
+if opciones_csv == 'Por defecto APP':
+    file = st.file_uploader("Elija un archivo csv con los resultados para realizar la revisión", type="csv", key="file1")
+    if file is not None:
+        df = pd.read_csv(file, sep=";", encoding="cp1252")
+        df= std_app(df)
+
+else:
+    file = st.file_uploader("Elija un archivo con los resultados para realizar la revisión",key="file2")
+    if file is not None:
+        df = pd.read_csv(file, sep=";", encoding="utf-8", header=None)
+        df = std_midas(df)
+        
+    
+if file is not None:
+    
+        st.markdown("#### Selecciona una fila para validar la ubicación resultante")
+        col1, col2 = st.columns(2)
+        gb = GridOptionsBuilder.from_dataframe(df)
+        gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
+        gb.configure_column("ubicación_válida", editable=True, cellRenderer=checkbox_renderer)
+        gb.configure_column("id", hide=True)
+        gb.configure_column("direccion", hide=True)
+        gb.configure_selection(selection_mode="single")
+        gb.configure_side_bar()
+        gridoptions = gb.build()
+
+        response = AgGrid(
+        df,
+        height=500,
+        width='100%',
+        gridOptions=gridoptions,
+        enable_enterprise_modules=True,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        fit_columns_on_grid_load=False,
+        header_checkbox_selection_filtered_only=True,
+        allow_unsafe_jscode=True,
+        use_checkbox=True)
+
+
+        v = response['selected_rows']
+            
+        if v:
+            dfs = pd.DataFrame(v)
+            direccion = dfs.iloc[0]["direccion_completa"]
+            st.markdown(f'##### Resultado de la dirección: {direccion}')
+            st.plotly_chart(display_map(dfs), use_container_width=True)
+
+        df_total = pd.DataFrame(response['data'])
+        df_val = df_total.loc[df_total["ubicación_válida"]]
+        df_noval = df_total.loc[~df_total["ubicación_válida"]]
+        with col1:
+            st.markdown(f"##### Ubicaciones validadas: {len(df_val.index)}")
+        with col2:
+            st.markdown(f"##### Ubicaciones no validadas: {len(df_noval.index)}")
+        csv_val = convert_df(df_val)
+        csv_noval = convert_df(df_noval)
+        st.download_button(
+            label="Descargue resultados validados",
+            data=csv_val,
+            file_name='resultados_válidos.csv',
+            mime='text/csv')
+        st.download_button(
+            label="Descargue resultados no validados",
+            data=csv_noval,
+            file_name='resultados_no_válidos.csv',
+            mime='text/csv')
+
+link_rev = '[Revision resultados](http://localhost:8501/Revision_resultados)'
+
+with st.sidebar:
+        st.markdown("#### Instrucciones de uso")
+        st.markdown(f"""##### 
+1) Seleccionar el tipo de archivo a cargar.
+2) Dar click en "Browse files" o en "Drag and drop file here" para cargar el archivo con los resultados.
+3) Se visualizará una tabla con los datos cargados. Por defecto, las ubicaciones que se definen como válidas (ticket azul) son:
+
+    a) Direcciones donde la comuna de entrada y la comuna georreferenciada coinciden.
+    
+    b) El tipo de ubicación coincide con los homólogos de mayor precisión de Google, los cuales son Rooftop y Range Interpolated.
+
+4) Para revisar en detalle la ubicación de un resultado, da click en una fila. Se visualizará un mapa con la ubicación de la dirección geolocalizada.
+5) En las filas de la columna "ubicación_valida" podrás prender el ticket azul para dejar una dirección válida. Lo contrario para dejar como no válida.
+
+6) Tras finalizar la revisión puedes descargar los datos validados y no validados por separado, dando click en los botones que se encuentran al final de la página.
+"""
+, unsafe_allow_html=True)
+        
