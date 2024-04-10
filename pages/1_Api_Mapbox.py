f705b24a7f9d08ef53d0427211b09804b822d916
Felipe Portales O <68240613+fportalesor@users.noreply.github.com>
2024-04-10 14:36:11 -0400

    Update 1_Api_Mapbox.py

--- pages/1_Api_Mapbox.py
+++ pages/1_Api_Mapbox.py
@@ -140,7 +140,7 @@ with container:
     if uploaded_file:
         df = pd.read_csv(uploaded_file, dtype=str, sep=";", encoding="latin-1")
 
-        region_select = st.sidebar.selectbox('Seleccione un Región:', options=opciones_region, format_func=lambda x: 'Región Metropolitana de Santiago' if x == '' else x)
+    region_select = st.selectbox('Seleccione un Región:', options=opciones_region, index=None)
     
     api_key_input = st.text_input(label="Ingrese API key", type="password")
     if api_key_input:
